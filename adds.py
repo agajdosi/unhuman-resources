92609c968a964f57ac07a22a5fbe8c2e6c3f5151
agajdosi <andreas.gajdosik@gmail.com>
2019-06-18 18:53:27 +0200

    Issue #2 Add a way to replace adds

--- adds.py
+++ adds.py
@@ -1,15 +1,14 @@
 import base64
+import random
 
 add = """
-<div style='position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:-1;background:url(data:image/gif;base64,the-image-data) no-repeat center center fixed;background-size:cover;'></div>
-"""
-
-add = """
-<div style='position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:-1;background:url(data:image/gif;base64,the-image-data) center center fixed;'></div>
+<div style='position:fixed;top:0;left:0;width:100vw;height:100vh;z-index:-1;background:url(data:image/gif;base64,the-image-data) no-repeat top center fixed;background-size:cover;'></div>
 """
 
 def addAdds(page):
-    with open("hackimages/1.png", "rb") as image_file:
+    x = random.randint(1, 2)
+    image_name = "hackimages/" + str(x) + ".jpg"
+    with open(image_name, "rb") as image_file:
         image = base64.b64encode(image_file.read())
         image = image.decode("utf-8")
 
