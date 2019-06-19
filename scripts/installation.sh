022d7b74a486bfe4a499e0a7a898431434c429ed
agajdosi <andreas.gajdosik@gmail.com>
2019-06-19 20:05:06 +0200

    Add scripts for installation and start of server

---
+++ scripts/installation.sh
@@ -0,0 +1,17 @@
+# INSTALL PREQS
+yum install centos-release-scl
+yum install rh-python36
+yum install git
+yum update -y nss curl libcurl
+
+# INSTALL PYTHON MODULES
+scl enable rh-python36 bash
+pip install tornado
+pip install requests
+pip install beautifulsoup4
+pip install tldextract
+
+# DOWNLOAD CODE
+adduser inovotna
+cd /home/inovotna
+git clone https://github.com/agajdosi/truednes.git
---
+++ scripts/startserver.sh
@@ -0,0 +1,3 @@
+# START SERVER
+scl enable rh-python36 bash
+python main.py -o idnes.cz -p 80 -n l-dnes.cz
