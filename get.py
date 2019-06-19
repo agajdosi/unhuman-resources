642fd068714556615012a293933f9b0d5b7e0106
agajdosi <andreas.gajdosik@gmail.com>
2019-06-19 17:30:01 +0200

    Handle link tag

--- get.py
+++ get.py
@@ -6,20 +6,23 @@ from adds import *
 def handleA(tag, originalAddress, newAddress):
     if tag.has_attr("href"):
         tag["href"] = replaceLink(tag["href"], originalAddress, newAddress)
-
-    return tag
-
-def handleMeta(tag, originalAddress, newAddress):
-    if tag.has_attr("content"):
-        tag["content"] = replaceLink(tag["content"], originalAddress, newAddress)
-
     return tag
 
 def handleBase(tag, originalAddress, newAddress):
     if tag.has_attr("href"):
         tag["href"] = replaceLink(tag["href"], originalAddress, newAddress)
-
     return tag
+
+def handleLink(tag, originalAddress, newAddress):
+    if tag.has_attr("href"):
+        tag["href"] = replaceLink(tag["href"], originalAddress, newAddress)
+    return tag
+
+def handleMeta(tag, originalAddress, newAddress):
+    if tag.has_attr("content"):
+        tag["content"] = replaceLink(tag["content"], originalAddress, newAddress)
+    return tag
+
 
 def downloadPage(url, headers):
     r = requests.get(url, headers=headers)
@@ -41,6 +44,8 @@ def getPage(url, originalAddress, newAddress, headers):
             tag = handleMeta(tag, originalAddress, newAddress)
         if tag.name == "base":
             tag = handleBase(tag, originalAddress, newAddress)
+        if tag.name == "link":
+            tag = handleLink(tag, originalAddress, newAddress)
                     
     #taeyyng
     page = str(soup)
