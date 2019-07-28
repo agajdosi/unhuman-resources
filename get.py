11e906873d573ca808f380541091e281f81438c0
agajdosi <andreas.gajdosik@gmail.com>
2019-07-28 14:24:00 +0200

    Add proxy support which was removed due to aiohttp

--- get.py
+++ get.py
@@ -3,49 +3,29 @@ import time
 from bs4 import BeautifulSoup
 from replace import *
 from fix import *
+import settings
 
 proxies = [
         {'proxy': "", "score": 200.0},
         {'proxy': "", "score": 200.0},
         {'proxy': "", "score": 200.0},
-        {'proxy': "", "score": 200.0},
-        {'proxy': "", "score": 200.0},
-        {'proxy': "socks4://91.217.96.25:56636", "score": 100.0},
-        {'proxy': "socks4://109.238.223.1:51372", "score": 100.0},
-        {'proxy': "socks4://62.201.17.103:51178", "score": 100.0},
-        {'proxy': "socks4://46.183.56.107:39799", "score": 100.0},
-        {'proxy': "socks4://82.142.70.244:4145", "score": 100.0},
-        {'proxy': "socks4://93.185.0.45:43398", "score": 100.0},
-        {'proxy': "socks4://185.131.62.244:10080", "score": 100.0},
-        {'proxy': "socks4://77.48.137.65:50523", "score": 100.0},
-        {'proxy': "socks4://109.238.208.138:51372", "score": 100.0},
-        {'proxy': "socks4://109.238.223.67:61150", "score": 100.0},
-        {'proxy': "socks4://84.242.123.220:4145", "score": 100.0},
-        {'proxy': "socks4://194.228.84.10:4145", "score": 100.0},
-        {'proxy': "socks4://91.217.96.1:56636", "score": 100.0},
-        {'proxy': "socks4://80.95.109.6:4145", "score": 100.0},
-        {'proxy': "socks4://93.99.193.13:46380", "score": 100.0},
-        {'proxy': "socks4://91.217.96.130:56636", "score": 100.0},
-        {'proxy': "socks4://89.102.198.78:45399", "score": 100.0},
-        {'proxy': "socks4://195.39.6.80:4145", "score": 100.0},
-        {'proxy': "socks4://93.99.193.45:37503", "score": 100.0},
-        {'proxy': "socks4://109.238.220.225:61221", "score": 100.0},
-        {'proxy': "socks4://90.181.150.211:4145", "score": 100.0},
-        {'proxy': "socks4://85.163.0.37:4145", "score": 100.0},
-        {'proxy': "socks4://93.99.53.201:48577", "score": 100.0},
-        {'proxy': "socks4://94.138.96.150:60883", "score": 100.0},
-        {'proxy': "socks4://85.135.95.218:4145", "score": 100.0},
-        {'proxy': "socks4://188.75.188.26:10801", "score": 100.0},
-        {'proxy': "socks4://77.48.29.70:45709", "score": 100.0},
-        {'proxy': "socks4://185.131.61.114:4145", "score": 100.0},
-        {'proxy': "socks4://89.24.119.126:4145", "score": 100.0},
-        {'proxy': "socks4://90.181.236.217:54495", "score": 100.0},
-        {'proxy': "socks4://109.238.222.1:4153", "score": 100.0},
-        {'proxy': "socks4://109.164.113.55:4145", "score": 100.0},
-        {'proxy': "socks4://217.77.171.114:4145", "score": 100.0},
-        {'proxy': "socks4://62.168.57.109:41983", "score": 100.0},
-        {'proxy': "socks4://82.142.87.2:4145", "score": 100.0}
+        {'proxy': "http://51.79.26.40:80", "score": 100.0},
+        {'proxy': "http://31.13.15.94:21776", "score": 100.0},
+        {'proxy': "http://46.246.16.3:8118", "score": 100.0},
+        {'proxy': "http://78.108.110.113:8080", "score": 100.0},
+        {'proxy': "http://83.167.253.143:9401	", "score": 100.0},
+        {'proxy': "http://78.41.19.181:8080", "score": 100.0},
+        {'proxy': "http://89.248.244.182:8080	", "score": 100.0},
+        {'proxy': "http://158.255.249.58:34593", "score": 100.0},
+        {'proxy': "http://198.50.147.158:3128", "score": 100.0},
+        {'proxy': "http://54.39.53.104:3128", "score": 100.0},
+        {'proxy': "http://198.211.102.155:8080", "score": 100.0},
+        {'proxy': "http://104.248.53.46:3128", "score": 100.0},
+        {'proxy': "http://136.243.47.220:3128", "score": 100.0},
+        {'proxy': "http://94.237.120.190:8080", "score": 100.0},
+        {'proxy': "http://173.249.53.235:3128", "score": 100.0},
           ]
+random.shuffle(proxies)
 
 def handleA(tag, originalAddress, newAddress):
     if tag.has_attr("href"):
@@ -73,32 +53,31 @@ def getProxyScore(item):
 async def downloadPage(url, headers):
     proxies.sort(key=getProxyScore, reverse=True)
     order = list(range(len(proxies)-1))
-    temp = order[:5]
+    temp = order[:3]
     random.shuffle(temp)
-    order[:5] = temp
+    order[:3] = temp
     tried = 0
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            r = await resp.text()
-    return r
     
-    """
     while True:
         #TODO handle absolute disaster: when tried > 100 report 404 or something
         try:
             x = tried % len(proxies)
             x = order[x]
-            proxy = {"http" : proxies[x]["proxy"], "https" : proxies[x]["proxy"]}
-            r = requests.get(url, headers=headers, proxies=proxy, timeout=2)
-            proxies[x]["score"] += 1
-            return r.text
+            if settings.args.debug == True:
+                print("using proxy: '{}'".format(proxies[x]["proxy"]))
+            
+            d = time.time()
+            async with session.get(url, timeout=5, proxy=proxies[x]["proxy"], headers=headers) as resp:
+                r = await resp.text()
+                print("clean download took",time.time()-d)
+                proxies[x]["score"] += 1
+                return r
         except:
             proxies[x]["score"] /= 2
-            print("proxy request failed:", proxy)
-        finally:
             tried += 1
-    """
+            if settings.args.debug == True:
+                print("proxy request failed: '{}'".format(proxies[x]["proxy"]))
+                print(proxies)
 
 async def getPage(url, originalAddress, newAddress, headers):
     start = time.time()
